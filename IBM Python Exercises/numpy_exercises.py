3588b3cf8d7120d3f3b3ae3b8de5e650a92f968f
André Silva <andrefcs@hotmail.com>
2020-12-15 18:31:54 -0300

    add numpy exercise

---
+++ IBM Python Exercises/numpy_exercises.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+np.array([1,-1])*np.array([1,1])
+
+np.dot(np.array([1,-1]),np.array([1,1]))
---
+++ IBM Python Exercises/pandas_exercise.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+"""df=pd.DataFrame({'a':[11,21,31],'b':[30,22,23]})
+df
+
+df[1,1]
+df.iloc[0,1]
+df.iloc[1,0] """
+
+""" import pandas as banana
+
+df=banana.DataFrame({'a':[11,21,31],'b':[21,22,23]})
+
+df.head() """
+
+""" df=pd.DataFrame({'a':[1,2,1],'b':[1,1,1]})
+
+df["a"]==1
+
+df.to_csv("file.csv") """
+
+df=pd.DataFrame({'a':[1,2,1],'b':[1,1,1]})
+
+df.head(df)

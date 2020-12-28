8a590a8ba845721674c1b8b09e6adf408081577d
André Silva <andrefcs@hotmail.com>
2020-12-28 12:37:04 -0300

    add numpy exercise

--- IBM Python Exercises/numpy_exercises.py
+++ IBM Python Exercises/numpy_exercises.py
@@ -1,5 +1,30 @@
 import numpy as np
 
-np.array([1,-1])*np.array([1,1])
+""" np.array([1,-1])*np.array([1,1])
 
-np.dot(np.array([1,-1]),np.array([1,1]))
+np.dot(np.array([1,-1]),np.array([1,1])) """
+
+A=np.array([[1,2],[3,4],[5,6],[7,8]])
+np.shape(A)
+
+A=np.array([[1,2],[3,4],[5,6],[7,8]])
+
+B=np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+np.dot(A,B)
+
+a=np.array([0,1,0,1,0])
+
+b=np.array([1,0,1,0,1])
+
+a*b
+
+a=np.array([0,1])
+
+b=np.array([1,0])
+
+np.dot(a,b)
+
+a=np.array([1,1,1,1,1])
+
+a+10
